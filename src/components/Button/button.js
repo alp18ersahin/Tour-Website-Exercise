b89c203c205def768523d0d9ed134a8c82784342
alpersahinalp <ersahinalp@gmail.com>
2024-07-10 01:40:56 +0300

    Update home page

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -2,7 +2,7 @@ export default function Button({text}) {
   return (
     <a
       href="#"
-      className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+      className="inline-block rounded-md border border-transparent bg-blue-500 px-8 py-3 text-center font-medium text-white hover:bg-blue-600"
     >
       {text}
     </a>
--- src/components/ImageGrid/imageGrid.js
+++ src/components/ImageGrid/imageGrid.js
@@ -10,37 +10,14 @@ className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:
       <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
         <img
           alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+          src="https://w0.peakpx.com/wallpaper/360/360/HD-wallpaper-sagrada-familia-basilica-of-the-holy-family-barcelona-catalonia-evening-sunset-cityscape-barcelona-landmark-basilica-de-la-sagrada-familia-roman-catholic-minor-basilica.jpg"
           className="h-full w-full object-cover object-center"
         />
       </div>
       <div className="h-64 w-44 overflow-hidden rounded-lg">
         <img
           alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
-          className="h-full w-full object-cover object-center"
-        />
-      </div>
-    </div>
-    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-      <div className="h-64 w-44 overflow-hidden rounded-lg">
-        <img
-          alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
-          className="h-full w-full object-cover object-center"
-        />
-      </div>
-      <div className="h-64 w-44 overflow-hidden rounded-lg">
-        <img
-          alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
-          className="h-full w-full object-cover object-center"
-        />
-      </div>
-      <div className="h-64 w-44 overflow-hidden rounded-lg">
-        <img
-          alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
+          src="https://images7.alphacoders.com/132/1327252.jpeg"
           className="h-full w-full object-cover object-center"
         />
       </div>
@@ -49,14 +26,37 @@ className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:
       <div className="h-64 w-44 overflow-hidden rounded-lg">
         <img
           alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
+          src="https://c4.wallpaperflare.com/wallpaper/697/782/487/palace-fortress-orange-sky-sunset-wallpaper-preview.jpg"
           className="h-full w-full object-cover object-center"
         />
       </div>
       <div className="h-64 w-44 overflow-hidden rounded-lg">
         <img
           alt=""
-          src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
+          src="https://cdn.pixabay.com/photo/2018/12/15/14/10/malaga-3876937_1280.jpg"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="h-64 w-44 overflow-hidden rounded-lg">
+        <img
+          alt=""
+          src="https://c4.wallpaperflare.com/wallpaper/522/985/271/alhambra-amanecer-ciudad-granada-wallpaper-preview.jpg"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+      <div className="h-64 w-44 overflow-hidden rounded-lg">
+        <img
+          alt=""
+          src="https://media.istockphoto.com/id/1148711252/photo/barcelona-aerial-view-from-the-high.webp?b=1&s=170667a&w=0&k=20&c=nAWmUOgx30FMDqW0y1tOGT2If9g9tFO797b2sg2bbLA="
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="h-64 w-44 overflow-hidden rounded-lg">
+        <img
+          alt=""
+          src="https://w0.peakpx.com/wallpaper/409/764/HD-wallpaper-seville-spain-seville-city-spain-square.jpg"
           className="h-full w-full object-cover object-center"
         />
       </div>
